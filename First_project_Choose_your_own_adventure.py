d251316ac524afcd047abcebcc44fbc87a74bc87
Jason <jason.woods435@gmail.com>
2023-02-12 15:45:14 +0000

    Fixed printing mistakes

--- First_project_Choose_your_own_adventure.py
+++ First_project_Choose_your_own_adventure.py
@@ -40,11 +40,11 @@ def game():
             print("You start with ", health, "health")
             print("Let's begin")
 
-            left_or_right = input("This one should be easy enough for you... Left or Right path? (left/right)")
+            left_or_right = input("This one should be easy enough for you... Left or Right path? (left/right) ")
             if left_or_right == "left":
-                ans = input("You follow a path and reach a large ominous castle... You going inside or following the path elsewhere? (castle/path)")
+                ans = input("You follow a path and reach a large ominous castle... You going inside or following the path elsewhere? (castle/path) ")
                 if ans == "path":
-                    path_ans = ("After following the path for a while you find a lake... swim to the other side or fish for dinner? (swim/fish)")
+                    path_ans = ("After following the path for a while you find a lake... swim to the other side or fish for dinner? (swim/fish) ")
                     if path_ans == "swim":
                         print("You haven't learn how to swim")
                         print("You drowned and died")
@@ -54,10 +54,10 @@ def game():
                     else: 
                         print("Guess who just died... You")
                 elif ans == "castle":
-                    castle_ans = input("You found a locked chest... kick it or take the stairs further up the castle? (Kick/stairs)")
+                    castle_ans = input("You found a locked chest... kick it or take the stairs further up the castle? (Kick/stairs) ")
                     if castle_ans == "kick":
                         print("The chest flies open, there's a white light...")
-                        print("You died)")
+                        print("You died")
                     elif castle_ans == "stairs":
                         print("You found Timmy's room")
                         print("He shoots you and you die")
@@ -66,7 +66,7 @@ def game():
                 else:
                     print("Guess who just died... You")
             else:
-                print("You Timmy found you and shot you")
+                print("Timmy found you and shot you")
                 print("You died on round 1... I should have known")
         else:
             print("Why did you bother wasting both of our times...")
